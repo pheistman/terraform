0607d4b942656925bfdccd459715072ebec64a97
pheistman <pheistman@live.co.uk>
2019-08-21 12:06:24 +0100

    initial commit

---
+++ pihole-droplet.tf
@@ -0,0 +1,12 @@
+resource "digitalocean_droplet" "pihole" {
+    name  = "pihole"
+    image = "ubuntu-16-04-x64"
+    region = "lon1"
+    size  = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_floating_ip" "ip" {
+    droplet_id = "${digitalocean_droplet.pihole.id}"
+    region     = "${digitalocean_droplet.pihole.region}"
+}
+
